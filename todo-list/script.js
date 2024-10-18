2b88c9c288ed1dbaa1c33a82292988f7b65bcc7c
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-10-18 10:37:36 +0530

    modified js code using event delegation

--- todo-list/script.js
+++ todo-list/script.js
@@ -6,14 +6,14 @@ para.style.fontWeight = "bold";
 para.style.fontSize = "30px";
 input.addEventListener("keydown", function (e) {
     if (e.key === "Enter" && `${input.value}` !== "") {
-        para.style.display="none";
+        para.style.display = "none";
         let div = document.createElement('div');
         let text = document.createElement('input');
         let editBtn = document.createElement('button');
         let deleteBtn = document.createElement('button');
         div.classList.add('each-task');
-        editBtn.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
-        deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        editBtn.innerHTML = `<i class="fa-solid fa-pencil edit"></i>`;
+        deleteBtn.innerHTML = '<i class="fas fa-trash-alt delete"></i>';
         text.setAttribute('readonly', true);
         const inputsize = input.value.length;
         text.setAttribute('size', inputsize)
@@ -25,30 +25,10 @@ input.addEventListener("keydown", function (e) {
         div.appendChild(editBtn)
         div.appendChild(deleteBtn)
         tasklist.appendChild(div);
-        editBtn.addEventListener("click", function () {
-            if (text.getAttribute('readonly')) {
-                editBtn.innerHTML = `<i class="fa-solid fa-save"></i>`;
-                text.removeAttribute('readonly');
-            }
-            else {
-                editBtn.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
-                text.setAttribute('readonly', true);
-                text.setAttribute('size', text.value.length);
-            }
 
-        })
-
-        deleteBtn.addEventListener("click", function (f) {
-            div.remove();
-            if (tasklist.querySelectorAll('.each-task').length === 0) {
-                tasklist.appendChild(para);
-                para.style.display="block";
-            }
-
-
-        })
-
+        input.value="";
     }
+    
 
 
 })
@@ -56,6 +36,33 @@ input.addEventListener("keydown", function (e) {
 if (tasklist.querySelectorAll('.each-task').length === 0) {
     tasklist.appendChild(para);
 }
+
+tasklist.addEventListener("click", function (e) {
+    if (e.target.classList.contains("edit")) {
+        let div = e.target.closest(".each-task");
+        let text = div.querySelector('.text')
+        if (text.getAttribute('readonly')) {
+            e.target.classList.remove('fa-pencil');
+            e.target.classList.add('fa-save');
+            text.removeAttribute('readonly');
+        }
+        else {
+            e.target.classList.add('fa-pencil');
+            e.target.classList.remove('fa-save');
+            text.setAttribute('readonly', true);
+            text.setAttribute('size', text.value.length);
+        }
+    }
+
+    if (e.target.classList.contains("delete")) {
+        let div = e.target.closest(".each-task");
+        div.remove();
+        if (tasklist.querySelectorAll('.each-task').length === 0) {
+            tasklist.appendChild(para);
+            para.style.display = "block";
+        }
+    }
+})
 
 
 
